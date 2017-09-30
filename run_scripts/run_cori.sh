066ced4eeeb9062eab8822979b2b31b20503aab0
Thorsten Kurth <thorstenkurth@me.com>
2017-09-29 17:10:30 -0700

    working benchmark. will add bigger, more scalable tests soon

--- run_scripts/run_cori.sh
+++ run_scripts/run_cori.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #SBATCH -p regular
 #SBATCH -A nstaff
-#SBATCH -N 9
+#SBATCH -N 1
 #SBATCH -C haswell
 #SBATCH -t 8:00:00
 #SBATCH -J hep_train_tf
 
 #set up python stuff
-module load python/3.6-anaconda-4.4
-module load h5py
-module load tensorflow/intel-head
+module load python
+source activate thorstendl-devel
+export PYTHONPATH=/cori_knl_small.json/usr/common/software/tensorflow/intel-tensorflow/head/lib/python2.7/site-packages
+#export PYTHONPATH=/global/homes/t/tkurth/python/tfzoo/tensorflow_mkl_hdf5_mpi_cw
 
 #run
-python ../scripts/hep_classifier_tf_train.py
+cd ../scripts/
+python hep_classifier_tf_train.py --config=../configs/cori_knl_small.json --num_tasks=1 --num_ps=0
