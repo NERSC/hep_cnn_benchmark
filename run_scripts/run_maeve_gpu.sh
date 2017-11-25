41bd0e01f16ce0808a831e41d479187fa7912a03
Thorsten Kurth <thorstenkurth@me.com>
2017-11-25 07:32:17 -0800

    local changed to config file

--- run_scripts/run_maeve_gpu.sh
+++ run_scripts/run_maeve_gpu.sh
@@ -16,7 +16,7 @@
 cd ../scripts/
 
 #set some parameters
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 #run the stuff
 python hep_classifier_tf_train.py --config=../configs/maeve_gpu_224.json --num_tasks=1 --num_ps=0
