6ef4d7e0da9cb0662fc936bb3e93d8042c84fd4e
Thorsten Kurth <thorstenkurth@me.com>
2018-09-03 11:42:17 -0700

    debugging

--- scripts/utils/split_root_dataset.py
+++ scripts/utils/split_root_dataset.py
@@ -4,6 +4,9 @@ import os
 import argparse
 import numpy as np
 import fnmatch as fn
+import pandas as pd
+import re
+import json
 #import root_numpy as rnp
 
 import glob
@@ -21,7 +24,11 @@ def parse_args():
     add_arg('--outputdir', type=str, required=True,
             help='Directories to write splitted data to')
     add_arg('--excludelist', type=str, default=None, required=False,
-            help='Text file with list of files to exclude (wildcards supported)')
+            help='Text file with list of files to exclude (wildcards supported). Takes precendence over include list.')
+    add_arg('--includelist', type=str, default=None, required=False,
+            help='Text file with list of files to include (wildcards supported). Superseded by include list.')
+    add_arg('--weightfile', type=str, default=None, required=True,
+            help='Text file containing a mapping of filenames to weights/cross sections')
     return parser.parse_args()
 
 def main():
@@ -40,42 +47,127 @@ def main():
             excludelist = [x.replace('\n','') for x in f.readlines()]
         print("Excluding ",excludelist)
     
+    #parse excludelist
+    includelist = None
+    if args.includelist:
+        with open(args.includelist,'r') as f:
+            includelist = [x.replace('\n','') for x in f.readlines()]
+        print("Including ",includelist)
+    
+    #load weightfile
+    weights={}
+    with open(args.weightfile, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        if not line.startswith('#'):
+            prefix, xsect = line.split()
+            weights[prefix] = float(xsect)
+    
     #parse input and shuffle
-    #signal
+    #--------------------------------------
+    #--------------- SIGNAL ---------------
+    #--------------------------------------
+    #initial file list
     signal_file_list = []
     for directory in args.inputdirs_signal:
         filelist = [os.path.join(directory,x) for x in os.listdir(directory) if x.endswith(".root")]
         signal_file_list += filelist
+    
+    #include list:
+    if includelist:
+        signal_file_list = list(filter(lambda x: any([fn.fnmatch(x,y) for y in includelist]), signal_file_list))
+    
     #remove stuff
     if excludelist:
-        signal_file_list = filter(lambda x: all([not fn.fnmatch(x,y) for y in excludelist]), signal_file_list)
-    signal_files = np.asarray(signal_file_list)
-    #shuffle
-    perm = np.random.permutation(len(signal_files))
-    signal_files = signal_files[perm]
+        signal_file_list = list(filter(lambda x: all([not fn.fnmatch(x,y) for y in excludelist]), signal_file_list))
     
-    #background
+    #add to dataframe
+    signaldf = pd.DataFrame(signal_file_list, columns=['filename'])
+    
+    #extract RPV params
+    tmpser = signaldf.apply(lambda x: re.findall(r'^RPV(\d{1,}?)_(\d{1,}?)_(\d{1,}?)-.*$', os.path.basename(x['filename'])), axis=1)
+    signaldf['rpv'] = tmpser.apply(lambda x: int(x['filename'][0]), axis=1)
+    signaldf['mglu'] = tmpser.apply(lambda x: int(x['filename'][1]), axis=1)
+    signaldf['mneu'] = tmpser.apply(lambda x: int(x['filename'][2]), axis=1)
+    
+    #merge in the weights
+    signaldf['weights'] = signaldf.apply(lambda x: weights[os.path.basename(x['filename']).split("-10k")[0]], axis=1)
+    
+    #sort by rpv params
+    signaldf.sort_values(by=['rpv','mglu','mneu'], inplace=True)
+    
+    #group and split
+    signaldf_train = pd.DataFrame(signaldf.groupby(['rpv','mglu','mneu']).apply(lambda x: x.iloc[0:int(x.shape[0]*args.split[0])]).reset_index(drop=True))
+    signaldf_train["phase"] = "training"
+    signaldf_validation = pd.DataFrame(signaldf.groupby(['rpv','mglu','mneu']).apply(lambda x: x.iloc[int(x.shape[0]*args.split[0]):int(x.shape[0]*args.split[0])+int(x.shape[0]*args.split[1])]).reset_index(drop=True))
+    signaldf_validation["phase"] = "validation"
+    signaldf_test = pd.DataFrame(signaldf.groupby(['rpv','mglu','mneu']).apply(lambda x: x.iloc[int(x.shape[0]*args.split[0])+int(x.shape[0]*args.split[1]):]).reset_index(drop=True))
+    signaldf_test["phase"] = "test"
+    #concatenate back
+    signaldf = pd.concat([signaldf_train, signaldf_validation, signaldf_test])
+    
+    #compute counts for all of them:
+    tmpc = pd.DataFrame(signaldf.groupby(['rpv','mglu','mneu', 'phase']).count().reset_index())
+    tmpc.rename(columns={'filename':'count'},inplace=True)
+    signaldf = signaldf.merge(tmpc[['rpv','mglu','mneu', 'phase', 'count']], on=['rpv','mglu','mneu', 'phase'], how='left')
+    
+    #--------------------------------------
+    #------------- Background -------------
+    #--------------------------------------
+    #initial file list
     background_file_list = []
     for directory in args.inputdirs_background:
         filelist = [os.path.join(directory,x) for x in os.listdir(directory) if x.endswith(".root")]
         background_file_list += filelist
+    
+    #include stuff
+    if includelist:
+        background_file_list = list(filter(lambda x: any([fn.fnmatch(x,y) for y in includelist]), background_file_list))
+    
     #remove stuff
     if excludelist:
-        background_file_list = filter(lambda x: all([not fn.fnmatch(x,y) for y in excludelist]), background_file_list)
-    background_files = np.asarray(background_file_list)
-    #shuffle
-    perm = np.random.permutation(len(background_files))
-    background_files = background_files[perm]
+        background_file_list = list(filter(lambda x: all([not fn.fnmatch(x,y) for y in excludelist]), background_file_list))
     
-    
-    #determine splits
-    #signal
-    num_train_signal = int(signal_files.shape[0]*args.split[0])
-    num_validation_signal = int(signal_files.shape[0]*args.split[1])
-    #background
-    num_train_background = int(background_files.shape[0]*args.split[0])
-    num_validation_background = int(background_files.shape[0]*args.split[1])
+    #add to dataframe
+    backgrounddf = pd.DataFrame(background_file_list, columns=['filename'])
 
+    #extract JZ params
+    tmpser = backgrounddf.apply(lambda x: re.findall(r'^QCDBkg_JZ(\d{1,}?)_(\d{1,}?)_(\d{1,}?)-.*$', os.path.basename(x['filename'])), axis=1)
+    backgrounddf['jz'] = tmpser.apply(lambda x: int(x['filename'][0]), axis=1).astype(int)
+    backgrounddf['pt_lo'] = tmpser.apply(lambda x: int(x['filename'][1]), axis=1).astype(int)
+    backgrounddf['pt_hi'] = tmpser.apply(lambda x: int(x['filename'][2]), axis=1).astype(int)
+    
+    #merge in the weights
+    backgrounddf['weights'] = backgrounddf.apply(lambda x: weights[os.path.basename(x['filename']).split("-10k")[0]], axis=1)
+    
+    #sort by those
+    backgrounddf.sort_values(by=['jz','pt_lo','pt_hi'], inplace=True)
+    
+    #group and split
+    backgrounddf_train = pd.DataFrame(backgrounddf.groupby(['jz','pt_lo','pt_hi']).apply(lambda x: x.iloc[0:int(x.shape[0]*args.split[0])]).reset_index(drop=True))
+    backgrounddf_train['phase'] = 'training'
+    backgrounddf_validation = pd.DataFrame(backgrounddf.groupby(['jz','pt_lo','pt_hi']).apply(lambda x: x.iloc[int(x.shape[0]*args.split[0]):int(x.shape[0]*args.split[0])+int(x.shape[0]*args.split[1])]).reset_index(drop=True))
+    backgrounddf_validation['phase'] = 'validation'
+    backgrounddf_test = pd.DataFrame(backgrounddf.groupby(['jz','pt_lo','pt_hi']).apply(lambda x: x.iloc[int(x.shape[0]*args.split[0])+int(x.shape[0]*args.split[1]):]).reset_index(drop=True))
+    backgrounddf_test['phase'] = 'test'
+    #concatenate back
+    backgrounddf = pd.concat([backgrounddf_train, backgrounddf_validation, backgrounddf_test])
+    
+    #compute counts for all of them:
+    tmpc = pd.DataFrame(backgrounddf.groupby(['jz','pt_lo','pt_hi', 'phase']).count().reset_index())
+    tmpc.rename(columns={'filename':'count'},inplace=True)
+    backgrounddf = backgrounddf.merge(tmpc[['jz','pt_lo','pt_hi', 'phase', 'count']], on=['jz', 'pt_lo', 'pt_hi', 'phase'], how='left')
+    
+    #--------------------------------------
+    #-------------- Weights ---------------
+    #--------------------------------------
+    #we need to divide the weights by the counts to get weights corresponding to the sample populations
+    signaldf['weights'] = signaldf['weights'] / signaldf['count'].astype(np.float32)
+    backgrounddf['weights'] = backgrounddf['weights'] / backgrounddf['count'].astype(np.float32)
+    
+    #--------------------------------------
+    #--------------- Output ---------------
+    #--------------------------------------
     #create output directories
     if not os.path.isdir(args.outputdir):
         os.makedirs(args.outputdir)
@@ -84,33 +176,42 @@ def main():
             if not os.path.isdir(os.path.join(args.outputdir,phase,label)):
                 os.makedirs(os.path.join(args.outputdir,phase,label))
     
-    #do the split
-    #signal
-    #training
-    for sourcename in signal_files[:num_train_signal]:
-        destname = os.path.join(args.outputdir, "training", "signal", os.path.basename(sourcename))
-        os.symlink(sourcename, destname)
-    #validation
-    for sourcename in signal_files[num_train_signal:num_train_signal+num_validation_signal]:
-        destname = os.path.join(args.outputdir, "validation", "signal", os.path.basename(sourcename))
-        os.symlink(sourcename, destname)
-    #test
-    for sourcename in signal_files[num_train_signal+num_validation_signal:]:
-        destname = os.path.join(args.outputdir, "test", "signal", os.path.basename(sourcename))
-        os.symlink(sourcename, destname)
-    #background
-    #training
-    for sourcename in background_files[:num_train_background]:
-        destname = os.path.join(args.outputdir, "training", "background", os.path.basename(sourcename))
-        os.symlink(sourcename, destname)
-    #validation
-    for sourcename in background_files[num_train_background:num_train_background+num_validation_background]:
-        destname = os.path.join(args.outputdir, "validation", "background", os.path.basename(sourcename))
-        os.symlink(sourcename, destname)
-    #test
-    for sourcename in background_files[num_train_background+num_validation_background:]:
-        destname = os.path.join(args.outputdir, "test", "background", os.path.basename(sourcename))
-        os.symlink(sourcename, destname)
+    #save new weights dictionary
+    #write out the files
+    for phase in ['training', 'validation', 'test']:
+        #signal
+        outpath = os.path.join(args.outputdir, phase, "signal")
+        selectdf = signaldf[ signaldf.phase == phase ]
+        #data
+        sourcefiles = selectdf['filename'].values
+        #create symlinks
+        for sourcename in sourcefiles:
+            destname = os.path.join(outpath, os.path.basename(sourcename))
+            os.symlink(sourcename, destname)
+        #extract weights dict
+        weightdf = pd.DataFrame(selectdf.groupby(['rpv', 'mglu', 'mneu']).apply(lambda x: x['weights'].iloc[0]).reset_index().rename(columns={0:'weights'}))
+        weightdf['prefix'] = weightdf.apply(lambda x: 'RPV{rpv:d}_{mglu:d}_{mneu:d}'.format(rpv=int(x['rpv']), mglu=int(x['mglu']), mneu=int(x['mneu'])), axis=1)
+        sweights = {x[0]: x[1] for x in weightdf[['prefix', 'weights']].values}
+            
+        #background
+        outpath = os.path.join(args.outputdir, phase, "background")
+        selectdf = backgrounddf[ backgrounddf.phase == phase ]
+        #data
+        sourcefiles = backgrounddf['filename'].values
+        #create symlinks
+        for sourcename in sourcefiles:
+            destname = os.path.join(outpath, os.path.basename(sourcename))
+            os.symlink(sourcename, destname)
+        
+        #extract weights dict
+        weightdf = pd.DataFrame(selectdf.groupby(['jz', 'pt_lo', 'pt_hi']).apply(lambda x: x['weights'].iloc[0]).reset_index().rename(columns={0:'weights'}))
+        weightdf['prefix'] = weightdf.apply(lambda x: 'QCDBkg_J{jz:d}_{ptl:d}_{pth:d}'.format(jz=int(x['jz']), ptl=int(x['pt_lo']), pth=int(x['pt_hi'])), axis=1)
+        bweights = {x[0]: x[1] for x in weightdf[['prefix', 'weights']].values}
+        
+        #store weight dicts:
+        outweights = {**sweights, **bweights}
+        with open(os.path.join(args.outputdir, phase, 'weights.json'), 'w') as f:
+            json.dump(outweights, f)
     
 if __name__ == '__main__':
     main()
